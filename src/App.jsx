1bbbb5abc2f53302c2de99becd51944d4bdf5c47
Marshall Brown <marshallendel@gmail.com>
2023-01-09 22:26:54 -0500

    decrement counter state

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count++)}>
+        <button onClick={() => setCount((count) => count--)}>
           count is {count}
         </button>
         <p>
