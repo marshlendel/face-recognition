795417f0c0040ad5b7c52244de7d5c2ef6e60cc2
Marshall Brown <marshallendel@gmail.com>
2023-08-09 21:16:37 -0400

    add navigation component

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,9 @@
+const Navigation = () => {
+    return (
+        <nav style={{display: "flex", justifyContent: "flex-end"}}>
+            <p>Sign Out</p>
+        </nav>
+    )
+}
+
+export default Navigation
